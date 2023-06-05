300744c531fba2e879f15043d2c2541c0d216113
Jose Pereda <joselpereda@hotmail.com>
2023-06-04 22:52:17 -0700

    final commit

--- public/employees.js
+++ public/employees.js
@@ -1,13 +1,76 @@
-let sally = 'Sally Smith';
-let mark = 'Mark Martin';
-let holly = 'Holly Unlikely';
-let amol = 'Amol Shookup';
-let robin = 'Robin Banks';
-let jose = 'Jose Pereda';
-const element = /*#__PURE__*/React.createElement("ul", {
-  style: {
-    'color': 'blue',
-    'fontSize': '24px'
+class BorderWrap extends React.Component {
+  render() {
+    const borderStyle = {
+      border: "1px solid silver",
+      padding: 6
+    };
+    return /*#__PURE__*/React.createElement("div", {
+      style: borderStyle
+    }, this.props.children);
   }
-}, /*#__PURE__*/React.createElement("li", null, sally), /*#__PURE__*/React.createElement("li", null, mark), /*#__PURE__*/React.createElement("li", null, holly), /*#__PURE__*/React.createElement("li", null, amol), /*#__PURE__*/React.createElement("li", null, robin), /*#__PURE__*/React.createElement("li", null, jose));
-ReactDOM.render(element, document.getElementById('content'));
+}
+class EmployeeFilter extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for the employee filter.");
+  }
+}
+class EmployeeTable extends React.Component {
+  render() {
+    const rowStyle = {
+      border: "1px solid silver",
+      padding: 4
+    };
+    return /*#__PURE__*/React.createElement("table", {
+      style: {
+        width: "100%",
+        borderCollapse: "collapse"
+      }
+    }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
+      style: rowStyle
+    }, "Name"), /*#__PURE__*/React.createElement("th", {
+      style: rowStyle
+    }, "Extension"), /*#__PURE__*/React.createElement("th", {
+      style: rowStyle
+    }, "Email"), /*#__PURE__*/React.createElement("th", {
+      style: rowStyle
+    }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(EmployeeRow, {
+      rowstyle: rowStyle,
+      employee_name: "Jose Pereda",
+      employee_ext: 5541,
+      employee_email: "joselpereda@live.com",
+      employee_title: "Chief Tech Architect"
+    }), /*#__PURE__*/React.createElement(EmployeeRow, {
+      rowstyle: rowStyle,
+      employee_name: "Tricia Banks",
+      employee_ext: 4553,
+      employee_email: "sallybanks@live.com",
+      employee_title: "M&O"
+    })));
+  }
+}
+class EmployeeRow extends React.Component {
+  render() {
+    const style = this.props.rowstyle;
+    return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+      style: style
+    }, this.props.employee_name), /*#__PURE__*/React.createElement("td", {
+      style: style
+    }, this.props.employee_ext), /*#__PURE__*/React.createElement("td", {
+      style: style
+    }, this.props.employee_email), /*#__PURE__*/React.createElement("td", {
+      style: style
+    }, this.props.employee_title));
+  }
+}
+class EmployeeAdd extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for a form to add a new employee.");
+  }
+}
+class EmployeeList extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(BorderWrap, null, /*#__PURE__*/React.createElement("h1", null, "Employee Management Application"), /*#__PURE__*/React.createElement(EmployeeFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeTable, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeAdd, null)));
+  }
+}
+const contentNode = document.getElementById('content');
+ReactDOM.render( /*#__PURE__*/React.createElement(EmployeeList, null), contentNode);
--- src/employees.jsx
+++ src/employees.jsx
@@ -1,17 +1,85 @@
-let sally = 'Sally Smith'
-let mark = 'Mark Martin'
-let holly = 'Holly Unlikely'
-let amol = 'Amol Shookup'
-let robin = 'Robin Banks'
-let jose = 'Jose Pereda'
-const element = (
-    <ul style={{'color':'blue', 'fontSize': '24px'}}>
-        <li>{sally}</li>
-        <li>{mark}</li>
-        <li>{holly}</li>
-        <li>{amol}</li>
-        <li>{robin}</li>
-        <li>{jose}</li>
-    </ul>
-)
-ReactDOM.render(element, document.getElementById('content'))
+class BorderWrap extends React.Component {
+    render() {
+        const borderStyle = {border: "1px solid silver", padding: 6}
+        return (
+             <div style={borderStyle}>
+                {this.props.children}
+             </div>
+        )
+    }
+}
+
+class EmployeeFilter extends React.Component {
+    render() {
+        return(<div>This is a placeholder for the employee filter.</div>)
+    } 
+}
+
+class EmployeeTable extends React.Component {
+    render() {
+        const rowStyle = {border: "1px solid silver", padding:4}
+        return(
+            <table style={{width:"100%", borderCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th style={rowStyle}>Name</th>
+                        <th style={rowStyle}>Extension</th>
+                        <th style={rowStyle}>Email</th>
+                        <th style={rowStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <EmployeeRow rowstyle={rowStyle}
+                        employee_name="Jose Pereda"
+                        employee_ext={5541}
+                        employee_email="joselpereda@live.com"
+                        employee_title="Chief Tech Architect"/>
+                    <EmployeeRow rowstyle={rowStyle}
+                        employee_name="Tricia Banks"
+                        employee_ext={4553}
+                        employee_email="sallybanks@live.com"
+                        employee_title="M&O"/>
+                </tbody>
+            </table>
+        )
+    } 
+}
+
+class EmployeeRow extends React.Component {
+    render () {
+        const style = this.props.rowstyle
+        return (<tr>
+            <td style={style}>{this.props.employee_name}</td>
+            <td style={style}>{this.props.employee_ext}</td>
+            <td style={style}>{this.props.employee_email}</td>
+            <td style={style}>{this.props.employee_title}</td>
+        </tr>
+        )
+    }
+}
+
+class EmployeeAdd extends React.Component {
+    render() {
+        return(<div>This is a placeholder for a form to add a new employee.</div>)
+    } 
+}
+
+class EmployeeList extends React.Component {
+    render() { 
+        return(
+            <React.Fragment>
+                <BorderWrap>
+                    <h1>Employee Management Application</h1> 
+                    <EmployeeFilter />
+                    <hr />
+                    <EmployeeTable/>
+                    <hr />
+                    <EmployeeAdd />
+                </BorderWrap>
+            </React.Fragment>
+        )
+    }
+}
+
+const contentNode = document.getElementById('content')
+ReactDOM.render(<EmployeeList />, contentNode)
